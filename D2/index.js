d224395bd98030e2869eda12cbb91097743ffb53
Trohat <petr.sed@centrum.cz>
2020-12-02 13:26:51 +0100

    Better variable names

--- D2/index.js
+++ D2/index.js
@@ -12,20 +12,20 @@ data = data.map(d => {
         low: +d.slice(0, hyphen),
         high: +d.slice(hyphen+1, space),
         condition: d.slice(space+1, colon),
-        pass: d.slice(colon+2)
+        password: d.slice(colon+2)
     }
 });
 
-const result1 = data.reduce((accumulator, value) =>  {
-    let count = value.pass.split(value.condition).length-1;
-    if (count >= value.low && count <= value.high) return accumulator + 1;
+const result1 = data.reduce((accumulator, currentValue) =>  {
+    let count = currentValue.password.split(currentValue.condition).length-1;
+    if (count >= currentValue.low && count <= currentValue.high) return accumulator + 1;
     return accumulator;
 },0);
 
-const result2 = data.reduce((accumulator, value) =>  {
-    let first = value.pass.charAt(value.low - 1);
-    let second = value.pass.charAt(value.high - 1);
-    return accumulator + (first === value.condition ^ second === value.condition);
+const result2 = data.reduce((accumulator, currentValue) =>  {
+    let firstChar = currentValue.password.charAt(currentValue.low - 1);
+    let secondChar = currentValue.password.charAt(currentValue.high - 1);
+    return accumulator + (firstChar === currentValue.condition ^ secondChar === currentValue.condition);
 },0);
 
 console.log("Task 1: " + result1);
