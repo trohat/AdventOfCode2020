adb99e9c0cce5114f5bc3e6b65ba1edb8db5c113
Trohat <petr.sed@centrum.cz>
2020-12-15 08:39:31 +0100

    Day 15 Task 2

--- D15/index.js
+++ D15/index.js
@@ -4,39 +4,85 @@ const splitLines = (data) => data.split(",");
 
 const prepare = data => data.map(d => +d);
 
-inputdata = prepare(splitLines(inputdata));
+preparedinputdata = prepare(splitLines(inputdata));
 
 console.log(inputdata);
 
 const task1 = (numbers) => {
     while( numbers.length < 2020) {
-        //const newNumber = numbers[numbers.length-1];
         const beforeLastMove = [ ...numbers];
         const newNumber = beforeLastMove.pop();
         const lastIndex = beforeLastMove.lastIndexOf(newNumber)
         if (lastIndex === -1) numbers.push(0);
         else numbers.push(numbers.length - lastIndex - 1);
     }
-    console.log(numbers);
     return numbers.pop();
 };
 
-const task2 = data => {
- 
+const task2slower = (numbers) => {
+    const spokenNumbers = new Map();
+    let lastIndex = numbers.length - 1
+    numbers.forEach((n, index) => {
+        if (index !== lastIndex) spokenNumbers.set(n, index);
+    });
+    let lastNumber = numbers[lastIndex];
+    let newNumber;
+    while( numbers.length < 30000000) {
+        if (!spokenNumbers.has(lastNumber)) newNumber = 0;
+        else newNumber = (numbers.length - spokenNumbers.get(lastNumber) - 1);
+        numbers.push(newNumber);
+        spokenNumbers.set(lastNumber, lastIndex);
+        lastIndex += 1;
+        lastNumber = newNumber;
+    }
+    return numbers.pop();
+};
+
+const task2 = (numbers) => {
+    const spokenNumbers = new Map();
+    let lastIndex = numbers.length - 1
+    numbers.forEach((n, index) => {
+        if (index !== lastIndex) spokenNumbers.set(n, index);
+    });
+    let lastNumber = numbers[lastIndex];
+    let newNumber;
+    while( lastIndex + 1 < 30000000) {
+        if (!spokenNumbers.has(lastNumber)) newNumber = 0;
+        else newNumber = (lastIndex - spokenNumbers.get(lastNumber));
+        spokenNumbers.set(lastNumber, lastIndex);
+        lastIndex += 1;
+        lastNumber = newNumber;
+    }
+    return lastNumber;
+};
+
+const tasktest = data => {
+    while (data.length < 30000000) {
+        data.push(1);
+    }
+    return data.pop();
 }
 
 let testdata = `0,3,6`;
 
-testdata = prepare(splitLines(testdata));
+console.log("");
+
+preparedtestdata = prepare(splitLines(testdata));
+
+doEqualTest(task1(preparedtestdata), 436);
+
+console.log("Task 1: " + task1(preparedinputdata));
 
 console.log("");
 
-doEqualTest(task1(testdata), 436);
+console.time();
 
-console.log("Task 1: " + task1(inputdata));
+preparedtestdata = prepare(splitLines(testdata));
 
-console.log("");
+preparedinputdata = prepare(splitLines(inputdata));
 
-//doEqualTest(task2(testdata), 336);
+doEqualTest(task2(preparedtestdata), 175594);
 
-//console.log("Task 2: " + task2(inputdata));
+console.log("Task 2: " + task2(preparedinputdata));
+
+console.timeEnd();
