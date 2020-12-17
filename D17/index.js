84a0bfd13b5450426f09826dfd61dbc88101cc38
Trohat <petr.sed@centrum.cz>
2020-12-17 08:29:21 +0100

    Day 17 Task 2

--- D17/index.js
+++ D17/index.js
@@ -5,12 +5,15 @@ const splitLines = (data) => data.split(String.fromCharCode(10));
 const prepare = data => {
     const makeGrid = dimension => {
         const grid = [];
-        for (let i = 0; i < zDimension; i++) {
+        for (let i = 0; i < yzDimension; i++) {
             grid.push([]);
-            for (let j = 0; j < dimension; j++) {
+            for (let j = 0; j < yzDimension; j++) {
                 grid[i].push([]);
                 for (let k = 0; k < dimension; k++) {
-                    grid[i][j].push(0);
+                    grid[i][j].push([]);
+                    for (let l = 0; l < dimension; l++) {
+                        grid[i][j][k].push(0);
+                    }
                 }
             }
         }
@@ -27,55 +30,64 @@ const prepare = data => {
             if (char === '#') char = 1;
             else if (char === '.') char = 0;
             else console.error("Unknown character");
-            grid[middleLayer][j+shift-centerShift][k+shift-centerShift] = char;
+            grid[middleLayer][middleLayer][j+shift-centerShift][k+shift-centerShift] = char;
         }
     }
     
     //console.log(grid);
     //console.log(grid[middleLayer]);
+    //console.log(grid[middleLayer][middleLayer]);
 
     return grid;
 };
 
 
-const task1 = (grid) => {
+const task2 = (grid) => {
     const deepCopy = grid => {
         const newGrid = [];
         for (let i = 0; i < grid.length; i++) {
             newGrid.push([]);
             for (let j = 0; j < grid[0].length; j++) { 
-                newGrid[i].push([ ...grid[i][j]]);
+                newGrid[i].push([]);
+                for (let k = 0; k < grid[0][0].length; k++) {
+                    newGrid[i][j].push([ ...grid[i][j][k]]);
+                }
             }
         }
         return newGrid;
     }
     
     for (let step = 0; step < 6; step++) {
+        console.log("step " + step);
         let newGrid = deepCopy(grid);
         
         for (let i = 1; i < grid.length - 1; i++) {
             for (let j = 1; j < grid[0].length - 1; j++) {
                 for (let k = 1; k < grid[0][0].length - 1; k++) {
+                    for (let l = 1; l < grid[0][0][0].length - 1; l++) {
                     
-                    let neighbors = 0;
-                    for (let z = -1; z < 2; z++) {
-                        for (let y = -1; y < 2; y++) {
-                            for (let x = -1; x < 2; x++) {
-                                if (z === 0 && y === 0 && x === 0) continue;
-                                neighbors += grid[z+i][y+j][x+k];
+                        let neighbors = 0;
+                        for (let z = -1; z < 2; z++) {
+                            for (let y = -1; y < 2; y++) {
+                                for (let x = -1; x < 2; x++) {
+                                    for (let w = -1; w < 2; w++) {
+                                        if (z === 0 && y === 0 && x === 0 && w === 0) continue;
+                                        neighbors += grid[z+i][y+j][x+k][w+l];
+                                    }
+                                }
                             }
                         }
-                    }
 
-                    let actCube = grid[i][j][k];
+                        let actCube = grid[i][j][k][l];
 
-                    if (actCube === 1) {
-                        if (neighbors === 2 || neighbors === 3) newGrid[i][j][k] = 1; else newGrid[i][j][k] = 0;
-                    } 
-                    else if (actCube === 0) {
-                        if (neighbors === 3) newGrid[i][j][k] = 1; else newGrid[i][j][k] = 0;
-                    } else console.error("Unknown state"); 
+                        if (actCube === 1) {
+                            if (neighbors === 2 || neighbors === 3) newGrid[i][j][k][l] = 1; else newGrid[i][j][k][l] = 0;
+                        } 
+                        else if (actCube === 0) {
+                            if (neighbors === 3) newGrid[i][j][k][l] = 1; else newGrid[i][j][k][l] = 0;
+                        } else console.error("Unknown state"); 
                     
+                    }
                 }
             }
         }
@@ -88,14 +100,14 @@ const task1 = (grid) => {
     for (let i = 1; i < grid.length - 1; i++) {
         for (let j = 1; j < grid[0].length - 1; j++) {
             for (let k = 1; k < grid[0][0].length - 1; k++) {
-                alive += grid[i][j][k];
+                for (let l = 1; l < grid[0][0][0].length - 1; l++) {
+                    alive += grid[i][j][k][l];
+                }
             }
         }
     }
     return alive;
 };
-
-const task2 = (data) => {};
 
 let testdata = `.#.
 ..#
@@ -105,11 +117,14 @@ const dimension = 20;
 const shift = 10;
 const centerShift = 3; 
 
-const zDimension = 16;
-const zShift = 8; 
+const yzDimension = 16;
+const yzShift = 8; 
 
 // dimensions set to minimum now!
 /*
+
+these dimensions are good only for test data
+
 const dimension = 20;
 const shift = 10;
 const centerShift = 3; 
@@ -117,17 +132,18 @@ const centerShift = 3;
 const zDimension = 12;
 const zShift = 6; 
 */
+
 inputdata = prepare(splitLines(inputdata));
-testdata = prepare(splitLines(testdata));
+//testdata = prepare(splitLines(testdata));
 
 console.log("");
 
-doEqualTest(task1(testdata), 112);
+//doEqualTest(task1(testdata), 112);
 
-console.log("Task 1: " + task1(inputdata));
+//console.log("Task 1: " + task1(inputdata));
 
 console.log("");
 
-//doEqualTest(task2(testdata), 336);
+//doEqualTest(task2(testdata), 848);
 
-//console.log("Task 2: " + task2(inputdata));
+console.log("Task 2: " + task2(inputdata));
