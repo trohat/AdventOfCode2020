54309b3db2f5f5d3ad135f38127745152dd3be69
Trohat <petr.sed@centrum.cz>
2020-12-23 10:48:13 +0100

    Day 23 Task 2 clean working solution

--- D23/index.js
+++ D23/index.js
@@ -2,130 +2,73 @@ console.log("funguju");
 
 const prepare = data => data.split("").map(d => +d);
 
-
-
-inputdata = prepare(inputdata);
-
-const task = (cups, cupsCount, repetitions, task) => {
-    const printCups = currentCup => {
-        let str = currentCup.value.toString();
-        printCup = currentCup.next;
-        while (printCup !== currentCup) {
-            str += "," + printCup.value;
-            printCup = printCup.next;
+const task = (cupsArray, cupsCount, repetitions, task) => {
+    const printCups = current => {
+        let first = current;
+        let str = current.toString();
+        while (cups.get(current) !== first) {
+            current = cups.get(current);
+            str += "," + current;
         }
         return str;
     }
 
-    // creating linked list
-    let lastCup;
-    let firstCup;
-    let start = true;
-    for (let n of cups) {
-        if (start) {
-            firstCup = {
-                value: n,
-                next: null,
-                previous: null
-            };
-            lastCup = firstCup;
-            start = false;
-        } else {
-            let cup = {
-                value: n,
-                next: null,
-                previous: lastCup
-            }
-            lastCup.next = cup;
-            lastCup = cup;
-        }
-    } 
-    for (let i = 10; i <= cupsCount; i++) {
-        let cup = {
-            value: i,
-            next: null,
-            previous: lastCup
-        }
-        lastCup.next = cup;
-        lastCup = cup;
+    const cups = new Map();
+    for (let i = 0; i < cupsArray.length-1; i++) {
+        cups.set(cupsArray[i], cupsArray[i+1])
     }
-    lastCup.next = firstCup;
-    firstCup.previous = lastCup;
+    
+    if (cupsCount > cupsArray.length) cups.set(cupsArray[cupsArray.length - 1], cupsArray.length + 1)
 
-    console.log(firstCup);
-    //console.log(printCups(firstCup));
-    let currentCup = firstCup;
-
+    for (let i = 10; i < cupsCount; i++) cups.set(i, i+1);
+    
+    if (cupsCount <= cupsArray.length) cups.set(cupsArray[cupsArray.length - 1], cupsArray[0]);
+    else cups.set(cupsCount, cupsArray[0]);
+    
+    let currentCup = cupsArray[0];
+    
+    printCups(currentCup);
+    
     for (let i = 0; i < repetitions; i++) {
-        let firstOfThree = currentCup.next;
-        let secondOfThree = firstOfThree.next;
-        let thirdOfThree = secondOfThree.next;
-        let three = [ firstOfThree.value, secondOfThree.value, thirdOfThree.value ];
-        currentCup.next = thirdOfThree.next;
-        thirdOfThree.next.previous = currentCup;
+        let firstOfThree = cups.get(currentCup);
+        let secondOfThree = cups.get(firstOfThree);
+        let thirdOfThree = cups.get(secondOfThree);
+        let three = [ firstOfThree, secondOfThree, thirdOfThree ];
+        cups.set(currentCup, cups.get(thirdOfThree));
         
-        let destination = currentCup.value - 1;
+        let destination = currentCup - 1;
         while (three.includes(destination) || destination === 0) {
             if (three.includes(destination)) destination--;
             if (destination === 0) destination = cupsCount;
         }
-
-        let destinationCupForwards = currentCup.next;
-        let destinationCupBackwards = currentCup.previous;
-        if (i % 10000 === 0) console.time("searching");
-        let searchingSteps = 0;
-        while (destinationCupForwards.value !== destination && destinationCupBackwards.value !== destination) {
-            destinationCupForwards = destinationCupForwards.next;
-            destinationCupBackwards = destinationCupBackwards.previous;
-            searchingSteps++;
-        }
-        if (i % 10000 === 0) console.timeEnd("searching");
-        if (i % 10000 === 0) console.log("searching steps", searchingSteps);
-
-        let destinationCup;
-        if (destinationCupForwards.value === destination) {
-            destinationCup = destinationCupForwards;
-        } else if (destinationCupBackwards.value === destination) {
-            destinationCup = destinationCupBackwards;
-        } else console.warn("Error in conditions");
-        //console.log("Searching steps", searchingSteps);
-
-        thirdOfThree.next = destinationCup.next;
-        destinationCup.next.previous = thirdOfThree;
         
-        destinationCup.next = firstOfThree;
-        firstOfThree.previous = destinationCup;
+        cups.set(thirdOfThree, cups.get(destination));
+        cups.set(destination, firstOfThree);
 
-        currentCup = currentCup.next;
-        if (i % 10000 === 0) console.log(i + " iteration steps");
-        //console.log("Move", (i+2) + ":" )
-        //console.log(printCups(currentCup));
+        currentCup = cups.get(currentCup);
+        if (i % 1000000 === 0) console.log(i + " iteration steps");
     }
     
-    while (currentCup.value !== 1) currentCup = currentCup.next;
-    if (task === 1) return printCups(currentCup).split(",").slice(1).join("");
-
-    currentCup = currentCup.next;
-    return currentCup.value * currentCup.next.value;
-
+    if (task === 1) return printCups(1).split(",").slice(1).join("");
+    
+    return cups.get(1) * cups.get(cups.get(1));
+    
 };
 
 let testdata = `389125467`;
 
 testdata = prepare(testdata);
 
+inputdata = prepare(inputdata);
 
-//doEqualTest(task(testdata, 9, 100, 1), "67384529");
+doEqualTest(task(testdata, 9, 100, 1), "67384529");
 
-//console.log("Task 1: " + task(inputdata, 9, 100, 1));
-
-console.log("");
-
-
+console.log("Task 1: " + task(inputdata, 9, 100, 1));
 
 console.log("");
 
-//doEqualTest(task(testdata, 1000000, 100000), 149245887792);
+doEqualTest(task(testdata, 1000000, 10000000), 149245887792);
+
 console.time();
 console.log("Task 2: " + task(inputdata, 1000000, 10000000));
 console.timeEnd();
