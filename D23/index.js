f00c7a4d5be79bfaf243233a7cb810ac1b1001df
Trohat <petr.sed@centrum.cz>
2020-12-23 12:18:50 +0100

    Day 23 Task 2 even faster solution

--- D23/index.js
+++ D23/index.js
@@ -6,35 +6,35 @@ const task = (cupsArray, cupsCount, repetitions, task) => {
     const printCups = current => {
         let first = current;
         let str = current.toString();
-        while (cups.get(current) !== first) {
-            current = cups.get(current);
+        while (cups[current] !== first) {
+            current = cups[current];
             str += "," + current;
         }
         return str;
     }
 
-    const cups = new Map();
+    const cups = {};
     for (let i = 0; i < cupsArray.length-1; i++) {
-        cups.set(cupsArray[i], cupsArray[i+1])
+        cups[cupsArray[i]] = cupsArray[i+1];
     }
     
-    if (cupsCount > cupsArray.length) cups.set(cupsArray[cupsArray.length - 1], cupsArray.length + 1)
+    if (cupsCount > cupsArray.length) cups[cupsArray[cupsArray.length - 1]] = cupsArray.length + 1;
 
-    for (let i = 10; i < cupsCount; i++) cups.set(i, i+1);
+    for (let i = 10; i < cupsCount; i++) cups[i] = i+1;
     
-    if (cupsCount <= cupsArray.length) cups.set(cupsArray[cupsArray.length - 1], cupsArray[0]);
-    else cups.set(cupsCount, cupsArray[0]);
+    if (cupsCount <= cupsArray.length) cups[cupsArray[cupsArray.length - 1]] = cupsArray[0];
+    else cups[cupsCount] = cupsArray[0];
     
     let currentCup = cupsArray[0];
     
     printCups(currentCup);
     
     for (let i = 0; i < repetitions; i++) {
-        let firstOfThree = cups.get(currentCup);
-        let secondOfThree = cups.get(firstOfThree);
-        let thirdOfThree = cups.get(secondOfThree);
+        let firstOfThree = cups[currentCup];
+        let secondOfThree = cups[firstOfThree];
+        let thirdOfThree = cups[secondOfThree];
         let three = [ firstOfThree, secondOfThree, thirdOfThree ];
-        cups.set(currentCup, cups.get(thirdOfThree));
+        cups[currentCup] = cups[thirdOfThree];
         
         let destination = currentCup - 1;
         while (three.includes(destination) || destination === 0) {
@@ -42,16 +42,16 @@ const task = (cupsArray, cupsCount, repetitions, task) => {
             if (destination === 0) destination = cupsCount;
         }
         
-        cups.set(thirdOfThree, cups.get(destination));
-        cups.set(destination, firstOfThree);
+        cups[thirdOfThree] = cups[destination];
+        cups[destination] = firstOfThree;
 
-        currentCup = cups.get(currentCup);
-        if (i % 1000000 === 0) console.log(i + " iteration steps");
+        currentCup = cups[currentCup];
+        //if (i % 1000000 === 0) console.log(i + " iteration steps");
     }
     
     if (task === 1) return printCups(1).split(",").slice(1).join("");
     
-    return cups.get(1) * cups.get(cups.get(1));
+    return cups[1] * cups[cups[1]];
     
 };
 
