b9d7192c7a0315826fd23b222eadd340b325bf3b
Trohat <petr.sed@centrum.cz>
2020-12-25 23:06:30 +0100

    Day 25 more accurate variable names

--- D25/index.js
+++ D25/index.js
@@ -9,9 +9,10 @@ inputdata = prepare(splitLines(inputdata));
 const task1 = (data) => {
     const getLoopSize = publicKey => {
         let loopSize = 0;
-        let subjectNumber = 1;
-        while (subjectNumber !== publicKey) {
-            subjectNumber = ( subjectNumber * 7 ) % 20201227;
+        let subjectNumber = 7;
+        let value = 1;
+        while (value !== publicKey) {
+            value = ( value * subjectNumber ) % 20201227;
             loopSize++;
         }
         console.log("Loop size:", loopSize);
